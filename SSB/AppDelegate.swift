4b7539c2eb086dac3de28e42a67e5289494180ca
Peter Upfold <pgithub@upfold.org.uk>
2020-04-26 14:15:18 +0100

    Initial Commit

---
+++ SSB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SSB
+//
+//  Created by Peter Upfold on 26/04/2020.
+//  Copyright © 2020 Peter Upfold. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SSB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SSB
+//
+//  Created by Peter Upfold on 26/04/2020.
+//  Copyright © 2020 Peter Upfold. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
